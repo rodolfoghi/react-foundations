b6511954afeb70c3eebb291476df69a2505ce015
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2024-04-18 23:32:10 -0300

    10. Server and Client Components

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import LikeButton from "./like-button";
 
 function Header({ title }) {
   return <h1>{title}</h1>;
@@ -6,11 +6,6 @@ function Header({ title }) {
 
 export default function HomePage() {
   const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
 
   return (
     <div>
@@ -20,7 +15,7 @@ export default function HomePage() {
           <li key={name}>{name}</li>
         ))}
       </ul>
-      <button onClick={handleClick}>Like({likes})</button>
+      <LikeButton />
     </div>
   );
 }
